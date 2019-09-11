1b9efda3440b8975f9298e6129b0a23f3d872ed7
KKMSoft-SourceCode <52309871+KKMSoft-SourceCode@users.noreply.github.com>
2019-09-11 12:35:53 +0530

    Biometric Reader

---
+++ Biometric Reader/BiometricReader.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Biometric_Reader
+{
+    partial class BiometricReader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BiometricReader));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnProceed = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnFolderBrowse = new System.Windows.Forms.Button();
+            this.txtOpDirectory = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtOpFileName = new System.Windows.Forms.TextBox();
+            this.lblOutputFileName = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtInputFilePath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnProceed);
+            this.groupBox1.Controls.Add(this.btnCancel);
+            this.groupBox1.Controls.Add(this.btnFolderBrowse);
+            this.groupBox1.Controls.Add(this.txtOpDirectory);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtOpFileName);
+            this.groupBox1.Controls.Add(this.lblOutputFileName);
+            this.groupBox1.Controls.Add(this.btnBrowse);
+            this.groupBox1.Controls.Add(this.txtInputFilePath);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(4, -2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(459, 151);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnProceed
+            // 
+            this.btnProceed.Location = new System.Drawing.Point(176, 117);
+            this.btnProceed.Name = "btnProceed";
+            this.btnProceed.Size = new System.Drawing.Size(75, 23);
+            this.btnProceed.TabIndex = 8;
+            this.btnProceed.Text = "Proceed";
+            this.btnProceed.UseVisualStyleBackColor = true;
+            this.btnProceed.Click += new System.EventHandler(this.btnProceed_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(267, 117);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 7;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnFolderBrowse
+            // 
+            this.btnFolderBrowse.Location = new System.Drawing.Point(416, 77);
+            this.btnFolderBrowse.Name = "btnFolderBrowse";
+            this.btnFolderBrowse.Size = new System.Drawing.Size(33, 21);
+            this.btnFolderBrowse.TabIndex = 6;
+            this.btnFolderBrowse.Text = "...";
+            this.btnFolderBrowse.UseVisualStyleBackColor = true;
+            this.btnFolderBrowse.Click += new System.EventHandler(this.btnFolderBrowse_Click);
+            // 
+            // txtOpDirectory
+            // 
+            this.txtOpDirectory.Location = new System.Drawing.Point(104, 78);
+            this.txtOpDirectory.Name = "txtOpDirectory";
+            this.txtOpDirectory.Size = new System.Drawing.Size(306, 20);
+            this.txtOpDirectory.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Output Directory :";
+            // 
+            // txtOpFileName
+            // 
+            this.txtOpFileName.Location = new System.Drawing.Point(104, 48);
+            this.txtOpFileName.Name = "txtOpFileName";
+            this.txtOpFileName.Size = new System.Drawing.Size(306, 20);
+            this.txtOpFileName.TabIndex = 3;
+            // 
+            // lblOutputFileName
+            // 
+            this.lblOutputFileName.AutoSize = true;
+            this.lblOutputFileName.Location = new System.Drawing.Point(8, 51);
+            this.lblOutputFileName.Name = "lblOutputFileName";
+            this.lblOutputFileName.Size = new System.Drawing.Size(90, 13);
+            this.lblOutputFileName.TabIndex = 2;
+            this.lblOutputFileName.Text = "Output Filename :";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(416, 16);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(33, 21);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtInputFilePath
+            // 
+            this.txtInputFilePath.Location = new System.Drawing.Point(104, 17);
+            this.txtInputFilePath.Name = "txtInputFilePath";
+            this.txtInputFilePath.Size = new System.Drawing.Size(306, 20);
+            this.txtInputFilePath.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select File :";
+            // 
+            // BiometricReader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 150);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "BiometricReader";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Biometric Reader";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInputFilePath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label lblOutputFileName;
+        private System.Windows.Forms.TextBox txtOpDirectory;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtOpFileName;
+        private System.Windows.Forms.Button btnFolderBrowse;
+        private System.Windows.Forms.Button btnProceed;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ Biometric Reader/BiometricReader.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Biometric_Reader
+{
+    public partial class BiometricReader : Form
+    {
+        public BiometricReader()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = false;
+            openFileDialog.Filter = ".CSV|*.CSV";
+            openFileDialog.ShowDialog();
+            if (string.IsNullOrWhiteSpace(openFileDialog.FileName))
+                return;
+            txtInputFilePath.Text = openFileDialog.FileName;
+            openFileDialog.Dispose();
+        }
+
+        private void btnFolderBrowse_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+            folderBrowser.ShowDialog();
+            if (string.IsNullOrWhiteSpace(folderBrowser.SelectedPath))
+                return;
+            txtOpDirectory.Text = folderBrowser.SelectedPath;
+            folderBrowser.Dispose();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnProceed_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtInputFilePath.Text) || (string.IsNullOrWhiteSpace(txtOpDirectory.Text)) || (string.IsNullOrWhiteSpace(txtOpFileName.Text)))
+                    MessageBox.Show("Field values should not be empty.", GetType().Name, MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+                var lines = File.ReadAllLines(txtInputFilePath.Text);
+
+
+
+                string reportFormat = string.Empty;
+                string durationFrom = string.Empty;
+
+
+                DataTable dt = new DataTable();
+                dt.Columns.Add("Card Id");
+                dt.Columns.Add("Employee Name");
+                dt.Columns.Add("Department");
+                dt.Columns.Add("Date");
+                dt.Columns.Add("First In");
+                dt.Columns.Add("Last Out");
+                dt.Columns.Add("Total Hours");
+
+                for (int i = 4; i < lines.Count(); i++)
+                {
+                    try
+                    {
+                        var details = lines[i].ToString().Split(',');
+                        if (details.Count() == 0 || details[0] == string.Empty)
+                            continue;
+
+                        string dateVal = details[3].Replace('"', ' ');
+                        if (!string.IsNullOrEmpty(dateVal) && !string.IsNullOrWhiteSpace(dateVal))
+                            dateVal = Convert.ToDateTime(dateVal).ToString("dd/MM/yyyy");
+
+                        string inTime = details[4].Replace('"', ' '); ;
+                        if (!string.IsNullOrEmpty(inTime) && !string.IsNullOrWhiteSpace(inTime))
+                            inTime = Convert.ToDateTime(inTime).ToString("HH:mm");
+
+                        string outTime = details[5].Replace('"', ' '); ;
+                        if (!string.IsNullOrEmpty(outTime) && !string.IsNullOrWhiteSpace(outTime))
+                            outTime = Convert.ToDateTime(outTime).ToString("HH:mm");
+
+                        dt.Rows.Add(details[0].Replace('"', ' '), details[1].Replace('"', ' '), details[2].Replace('"', ' '), dateVal, inTime, outTime, details[6].Replace('"', ' '));
+
+                    }
+                    catch { }
+                }
+
+                if (dt.Rows.Count == 0)
+                    return;
+
+                // xlApp.Quit();
+
+                WriteCSV(dt, "Format1");
+                WriteCSV(dt, "Format2");
+
+                MessageBox.Show("Successfully generated." + Environment.NewLine + "Output File - Format1  exists in :" + Path.Combine(txtOpDirectory.Text, txtOpFileName.Text + "_Format1.csv") + Environment.NewLine + "Output File - Format2 exists in " + Path.Combine(txtOpDirectory.Text, txtOpFileName.Text + "_Format2.csv"), GetType().Name, MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                txtInputFilePath.Clear();
+                txtOpDirectory.Clear();
+                txtOpFileName.Clear();
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+
+        private void WriteCSV(DataTable dt, string format)
+        {
+            StringBuilder sbCSV = new StringBuilder();
+
+            //sbCSV.Append(reportFormat.Substring(reportFormat.IndexOf(":"), reportFormat.Length - reportFormat.IndexOf(":")));
+            //sbCSV.Append(durationFrom.Substring(durationFrom.LastIndexOf(":"), durationFrom.Length - durationFrom.LastIndexOf(":")));
+
+            //sbCSV.Append(Environment.NewLine);
+            if (format.Equals("Format1"))
+                sbCSV.Append("Employee Number,In time with date,Out time with date,Date");
+            else
+                sbCSV.Append("Employee Number,Punch DateTime,Type");
+            sbCSV.Append(Environment.NewLine);
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                if (string.IsNullOrWhiteSpace(dr["Card Id"].ToString()))
+                    continue;
+                if (format.Equals("Format1"))
+                    sbCSV.Append(string.Format("{0},{1} {2},{3} {4},{5}", dr["Card Id"].ToString(), !string.IsNullOrWhiteSpace(dr["Date"].ToString()) ? Convert.ToDateTime(dr["Date"].ToString()).ToString("dd-MMM-yyyy") : string.Empty, !string.IsNullOrWhiteSpace(dr["First In"].ToString()) ? Convert.ToDateTime(dr["First In"].ToString()).ToString("HH:mm") : string.Empty, Convert.ToDateTime(dr["Date"].ToString()).ToString("dd-MMM-yyyy"), !string.IsNullOrWhiteSpace(dr["Last Out"].ToString()) ? Convert.ToDateTime(dr["Last Out"].ToString()).ToString("HH:mm") : string.Empty, !string.IsNullOrWhiteSpace(dr["Date"].ToString()) ? Convert.ToDateTime(dr["Date"].ToString()).ToString("dd-MMM-yyyy") : string.Empty));
+                else
+                {
+                    sbCSV.Append(string.Format("{0},{1} {2},{3}", dr["Card Id"].ToString(), !string.IsNullOrWhiteSpace(dr["Date"].ToString()) ? Convert.ToDateTime(dr["Date"].ToString()).ToString("dd-MMM-yyyy") : string.Empty, !string.IsNullOrWhiteSpace(dr["First In"].ToString()) ? Convert.ToDateTime(dr["First In"].ToString()).ToString("HH:mm") : string.Empty, "IN"));
+                    sbCSV.Append(Environment.NewLine);
+                    sbCSV.Append(string.Format("{0},{1} {2},{3}", dr["Card Id"].ToString(), !string.IsNullOrWhiteSpace(dr["Date"].ToString()) ? Convert.ToDateTime(dr["Date"].ToString()).ToString("dd-MMM-yyyy") : string.Empty, !string.IsNullOrWhiteSpace(dr["Last Out"].ToString()) ? Convert.ToDateTime(dr["Last Out"].ToString()).ToString("HH:mm") : string.Empty, "OUT"));
+                }
+                sbCSV.Append(Environment.NewLine);
+            }
+
+            sbCSV.Append(Environment.NewLine);
+
+            File.WriteAllText(Path.Combine(txtOpDirectory.Text, txtOpFileName.Text + "_" + format + ".csv"), sbCSV.ToString());
+
+            sbCSV.Clear();
+        }
+    }
+}
